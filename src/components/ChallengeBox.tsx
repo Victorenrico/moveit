c6a5a71412e57ea3fb9a4b5b807bc2fcfc3eaf39
Victor Codonho <victorenrico50@gmail.com>
2021-03-14 21:25:54 -0300

    Contexto countdown e notificações

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,22 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-    const { activeChallenges, resetChallenge } = useContext(ChallengesContext);
+    const { activeChallenges, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+    const { resetCountDown } = useContext(CountdownContext);
+
+    function handleChallengeSucceeded() {
+        completeChallenge();
+        resetCountDown();
+    }
+
+    function handleChallengeFailed() {
+        resetChallenge();
+        resetCountDown();
+    }
 
     return(
         <div className={styles.challengeBoxContainer}>
@@ -22,13 +34,14 @@ export function ChallengeBox() {
                         <button
                             type="button"
                             className={styles.callengeFailedButton}
-                            onClick={resetChallenge}
+                            onClick={handleChallengeFailed}
                         >
                             Falhei
                         </button>
                         <button
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={handleChallengeSucceeded}
                         >
                             Completei
                         </button>
